7dc8ea84f07b06662252a754141f94960ed5a05d
mtagius <mtagius@gmail.com>
2018-04-30 12:47:50 -0400

    added Arduino code and Gerber files for the PCB

---
+++ Magspoof-49th-SD.ino
@@ -0,0 +1,213 @@
+/*
+ * MagSpoof - "wireless" magnetic stripe/credit card emulator
+ * 
+ * This code was originally written by Samy Kamkar.
+ * http://samy.pl/magspoof/
+ *
+ * I have modified his code to run on the Adafriut Trinket.
+ * Some modifications were also made to make this more suitable for a Magspoof building workshop.
+ * 
+ * Check out the Github:
+ * https://github.com/mtagius/magspoof-49th-SD
+ *
+ */
+
+#define PIN_A 0
+#define PIN_B 2
+#define indicatorLED 1
+#define CLOCK_US 200
+
+#define BETWEEN_ZERO 53 // 53 zeros between track1 & 2
+
+// This is where YOUR card data needs to go
+const char* tracks[] = {
+"", // Track 1
+"" // Track 2
+};
+
+//This is how the tracks look for a student ID
+/*
+ * "%800123456?\0", // Track 1
+ * ";1234567890123456?\0" // Track 2
+*/
+
+//This is how the tracks look for a generic credit card
+/*
+ * "%B123456781234567^LASTNAME/FIRST^YYMMSSSDDDDDDDDDDDDDDDDDDDDDDDDD?\0", // Track 1
+ * ";123456781234567=YYMMSSSDDDDDDDDDDDDDD?\0" // Track 2
+ */
+
+char revTrack[41];
+
+const int sublen[] = {
+  32, 48, 48 };
+const int bitlen[] = {
+  7, 5, 5 };
+
+unsigned int curTrack = 0;
+int dir;
+
+void setup()
+{
+  pinMode(PIN_A, OUTPUT);
+  pinMode(PIN_B, OUTPUT);
+
+  pinMode(indicatorLED, OUTPUT);
+
+  // store reverse track 2 to play later
+  storeRevTrack(2);
+
+  for(int i = 0; i < 3; i++) {
+    //everytime playtrack(1) is called the magspoof outputs track 1 and 2
+    //calling this function will output the card stored in tracks[]
+    playTrack(1);
+    delay(2000);
+  }
+}
+
+// send a single bit out
+void playBit(int sendBit)
+{
+  dir ^= 1;
+  digitalWrite(PIN_A, dir);
+  digitalWrite(PIN_B, !dir);
+  delayMicroseconds(CLOCK_US);
+
+  if (sendBit)
+  {
+    dir ^= 1;
+    digitalWrite(PIN_A, dir);
+    digitalWrite(PIN_B, !dir);
+  }
+  delayMicroseconds(CLOCK_US);
+
+}
+
+// when reversing
+void reverseTrack(int track)
+{
+  int i = 0;
+  track--; // index 0
+  dir = 0;
+
+  while (revTrack[i++] != '\0');
+  i--;
+  while (i--)
+    for (int j = bitlen[track]-1; j >= 0; j--)
+      playBit((revTrack[i] >> j) & 1);
+}
+
+// plays out a full track, calculating CRCs and LRC
+void playTrack(int track)
+{
+  int tmp, crc, lrc = 0;
+  track--; // index 0
+  dir = 0;
+
+  digitalWrite(indicatorLED, HIGH);
+
+
+  // First put out a bunch of leading zeros.
+  for (int i = 0; i < 25; i++)
+    playBit(0);
+
+  //
+  for (int i = 0; tracks[track][i] != '\0'; i++)
+  {
+    crc = 1;
+    tmp = tracks[track][i] - sublen[track];
+
+    for (int j = 0; j < bitlen[track]-1; j++)
+    {
+      crc ^= tmp & 1;
+      lrc ^= (tmp & 1) << j;
+      playBit(tmp & 1);
+      tmp >>= 1;
+    }
+    playBit(crc);
+  }
+
+  // finish calculating and send last "byte" (LRC)
+  tmp = lrc;
+  crc = 1;
+  for (int j = 0; j < bitlen[track]-1; j++)
+  {
+    crc ^= tmp & 1;
+    playBit(tmp & 1);
+    tmp >>= 1;
+  }
+  playBit(crc);
+
+  // if track 1, play 2nd track in reverse (like swiping back?)
+  if (track == 0)
+  {
+    // if track 1, also play track 2 in reverse
+    // zeros in between
+    for (int i = 0; i < BETWEEN_ZERO; i++)
+      playBit(0);
+
+    // send second track in reverse
+    reverseTrack(2);
+  }
+
+  // finish with 0's
+  for (int i = 0; i < 5 * 5; i++)
+    playBit(0);
+
+  digitalWrite(PIN_A, LOW);
+  digitalWrite(PIN_B, LOW);
+
+  digitalWrite(indicatorLED, LOW);
+
+}
+
+// stores track for reverse usage later
+void storeRevTrack(int track)
+{
+  int i, tmp, crc, lrc = 0;
+  track--; // index 0
+  dir = 0;
+
+  for (i = 0; tracks[track][i] != '\0'; i++)
+  {
+    crc = 1;
+    tmp = tracks[track][i] - sublen[track];
+
+    for (int j = 0; j < bitlen[track]-1; j++)
+    {
+      crc ^= tmp & 1;
+      lrc ^= (tmp & 1) << j;
+      tmp & 1 ?
+        (revTrack[i] |= 1 << j) :
+        (revTrack[i] &= ~(1 << j));
+      tmp >>= 1;
+    }
+    crc ?
+      (revTrack[i] |= 1 << 4) :
+      (revTrack[i] &= ~(1 << 4));
+  }
+
+  // finish calculating and send last "byte" (LRC)
+  tmp = lrc;
+  crc = 1;
+  for (int j = 0; j < bitlen[track]-1; j++)
+  {
+    crc ^= tmp & 1;
+    tmp & 1 ?
+      (revTrack[i] |= 1 << j) :
+      (revTrack[i] &= ~(1 << j));
+    tmp >>= 1;
+  }
+  crc ?
+    (revTrack[i] |= 1 << 4) :
+    (revTrack[i] &= ~(1 << 4));
+
+  i++;
+  revTrack[i] = '\0';
+}
+
+void loop()
+{
+  //no code to loop. All code is run from setup()
+}
+
